c0f41b36956cd53c7ae6c6fa3b0eaf1aea266294
jamesdidathing <jamesalexhodson@gmail.com>
2025-12-02 13:24:26 +1100

    added date filter for blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,19 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
+
+   // Date filters for blog
+  eleventyConfig.addFilter("dateIso", (date) => {
+    return new Date(date).toISOString();
+  });
+
+  eleventyConfig.addFilter("dateReadable", (date) => {
+    return new Date(date).toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    });
+  });
   
   return {
     dir: {
